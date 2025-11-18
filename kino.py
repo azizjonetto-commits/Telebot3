afaaf67308b1938229b8dae1317c76a91f839b83
azizjonetto-commits <azizjonetto@gmail.com>
2025-11-18 20:01:05 +0500

    faylni qo'shish

---
+++ kino.py
@@ -0,0 +1,172 @@
+from telebot import TeleBot, types
+
+bot = TeleBot("7733509918:AAHwVCXFEN4s2OqBbagtO9JyBw7b5QvYxjA")
+
+CHANNELS = [
+    "@Pictures_16K"
+]
+
+def check_user(user_id):
+    for ch in CHANNELS:
+        try:
+            status = bot.get_chat_member(ch, user_id).status
+            if status in ('left', 'kichked'):
+                return False
+        except:
+            return False
+    return True 
+
+def IsSubscribe(chat_id):
+    markup = types.InlineKeyboardMarkup()
+    for ch in CHANNELS:
+        markup.add(types.InlineKeyboardButton(text=ch, url=f"https://t.me/{ch[1:]}"))
+    
+    markup.add(types.InlineKeyboardButton("✅Tekshirish✅", callback_data="check"))
+
+    bot.send_message(chat_id, "Botdan foydalanish uchun kanallarga obuna bo'lishiniz kerak!", reply_markup=markup)
+
+def kinolar(message):
+    bot.reply_to(message,f"""Salom!Men Animelarni qisqa treyleylarini topib beruvchi botman.""")
+
+    markup = types.InlineKeyboardMarkup()
+
+    button1 = types.InlineKeyboardButton("Yolg'izlikda daraja ko'tarish", callback_data="a")
+    button2 = types.InlineKeyboardButton("Ko'k zindon", callback_data="b")
+    button3 = types.InlineKeyboardButton("Soyada ko'tarilish", callback_data="c")
+    button4 = types.InlineKeyboardButton("Jodugarlar jangi", callback_data="d")
+    button5 = types.InlineKeyboardButton("Iblis qotili", callback_data="e")
+    button6 = types.InlineKeyboardButton("Naruto", callback_data="f")
+    button7 = types.InlineKeyboardButton("Quvib o'tish", callback_data="g")
+    button8 = types.InlineKeyboardButton("Shamolni bo'ysundurish", callback_data="h")
+    button9 = types.InlineKeyboardButton("Xarobalar qirolligi", callback_data="i")
+    button10 = types.InlineKeyboardButton("Titanlar hujumi", callback_data="j")
+
+
+    markup.add(button1,button2)
+    markup.add(button3)
+    markup.add(button4)
+    markup.add(button5)
+    markup.add(button6)
+    markup.add(button7)
+    markup.add(button8)
+    markup.add(button9)
+    markup.add(button10)
+
+    bot.send_message(message.chat.id, "Buttonlardan birini tanlang", reply_markup=markup)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    user_id = message.from_user.id
+    if check_user(user_id):
+        bot.send_message(message.chat.id, "Botdan foydalanishingiz mumikin!")
+        kinolar(message)
+    else:
+        IsSubscribe(message.chat.id)
+
+@bot.callback_query_handler(func=lambda call: call.data == "check")
+def check_callback(call):
+    user_id = call.from_user.id
+    if check_user(user_id):
+        bot.send_message(call.message.chat.id, "Botdan foydalanishingiz mumkin.")
+        bot.answer_callback_query(call.id)
+    else:
+        bot.send_message(call.message.chat.id, "Barcha kanallarga obuna bo'lmagansiz!\nOBUNA BO'LING!")
+        bot.answer_callback_query(call.id)
+
+@bot.message_handler(func=lambda message: True)
+def send(message):
+    user_id = message.from_user.id
+    if not check_user(user_id):
+        IsSubscribe(message.chat.id)
+        return
+
+    if message.text == "/1":
+        a = open("s.mp4","rb")
+        bot.send_video(message.chat.id, a)
+        bot.answer_callback_query(call.id)
+    elif message.text == "/2":
+        b = open("Blue Lock.mp4","rb")
+        bot.send_video(message.chat.id, b)
+        bot.answer_callback_query(call.id)
+    elif message.text == "/3":
+        c = open("Shadow.mp4","rb")
+        bot.send_video(message.chat.id, c)
+        bot.answer_callback_query(call.id)
+    elif message.text == "/4":
+        d = open("Jodugarlar Jangi.mp4","rb")
+        bot.send_video(message.chat.id, d)
+        bot.answer_callback_query(call.id)
+    elif message.text == "/5":
+        e = open("Iblis qotili.mp4","rb")
+        bot.send_video(message.chat.id, e)
+        bot.answer_callback_query(call.id)
+    elif message.text == "/6":
+        f = open("Naruto.mp4","rb")
+        bot.send_video(message.chat.id, f)
+        bot.answer_callback_query(call.id)
+    elif message.text == "/7":
+        g = open("Quvib o'tish.mp4","rb")
+        bot.send_video(message.chat.id, g)
+        bot.answer_callback_query(call.id)
+    elif message.text == "/8":
+        h = open("Shamolni o'rgatish.mp4","rb")
+        bot.send_video(message.chat.id, h)
+        bot.answer_callback_query(call.id)
+    elif message.text == "/9":
+        i = open("Xarobalar qirolligi.mp4","rb")
+        bot.send_video(message.chat.id, i)
+        bot.answer_callback_query(call.id)
+    elif message.text == "/10":
+        j = open("Titanlar hujumi.mp4","rb")
+        bot.send_video(message.chat.id, j)
+        bot.answer_callback_query(call.id)
+
+@bot.callback_query_handler(func=lambda callback:True)
+def nimadir(call):
+    user_id = call.from_user.id
+    if check_user(user_id):
+        if call.data == "a":
+            a = open("s.mp4","rb")
+            bot.send_video(call.message.chat.id, a)
+            bot.answer_callback_query(call.id)
+        elif call.data == "b":
+            b = open("Blue Lock.mp4","rb")
+            bot.send_video(call.message.chat.id, b)
+            bot.answer_callback_query(call.id)
+        elif call.data == "c":
+            c = open("Shadow.mp4","rb")
+            bot.send_video(call.message.chat.id, c)
+            bot.answer_callback_query(call.id)
+        elif call.data == "d":
+            d = open("Jodugarlar Jangi.mp4","rb")
+            bot.send_video(call.message.chat.id, d)
+            bot.answer_callback_query(call.id)   
+        elif call.data == "e":
+            e = open("Iblis qotili.mp4","rb")
+            bot.send_video(call.message.chat.id, e)
+            bot.answer_callback_query(call.id)      
+        elif call.data == "f":
+            f = open("Naruto.mp4","rb")
+            bot.send_video(call.message.chat.id, f)
+            bot.answer_callback_query(call.id)  
+        elif call.data == "g":
+            g = open("Quvib o'tish.mp4","rb")
+            bot.send_video(call.message.chat.id, g)
+            bot.answer_callback_query(call.id) 
+        elif call.data == "h":
+            h = open("Shamolni o'rgatish.mp4","rb")
+            bot.send_video(call.message.chat.id, h)
+            bot.answer_callback_query(call.id)
+        elif call.data == "i":
+            i = open("Xarobalar qirolligi.mp4","rb")
+            bot.send_video(call.message.chat.id, i)
+            bot.answer_callback_query(call.id)   
+        elif call.data == "j":
+            j = open("Titanlar hujumi.mp4","rb")
+            bot.send_video(call.message.chat.id, j)
+            bot.answer_callback_query(call.id)
+    else:
+        IsSubscribe(call.message.chat.id)
+
+print("Iwladi")
+bot.infinity_polling()
