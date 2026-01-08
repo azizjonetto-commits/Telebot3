cf6a9be8437edf791ff1329f2f5b153076c6cd28
azizjonetto-commits <azizjonetto@gmail.com>
2026-01-08 20:28:29 +0500

    kinochi bot

--- kino.py
+++ kino.py
@@ -1,32 +1,34 @@
-from telebot import TeleBot, types
+from telebot import types
+import asyncio 
+from telebot.async_telebot import AsyncTeleBot
 
-bot = TeleBot("7733509918:AAHwVCXFEN4s2OqBbagtO9JyBw7b5QvYxjA")
+bot = AsyncTeleBot("7733509918:AAHwVCXFEN4s2OqBbagtO9JyBw7b5QvYxjA")
 
 CHANNELS = [
     "@Pictures_16K"
 ]
 
-def check_user(user_id):
+async def check_user(user_id):
     for ch in CHANNELS:
         try:
-            status = bot.get_chat_member(ch, user_id).status
+            status = await bot._chat_memberget(ch, user_id).status
             if status in ('left', 'kichked'):
                 return False
         except:
             return False
     return True 
 
-def IsSubscribe(chat_id):
+async def IsSubscribe(chat_id):
     markup = types.InlineKeyboardMarkup()
     for ch in CHANNELS:
         markup.add(types.InlineKeyboardButton(text=ch, url=f"https://t.me/{ch[1:]}"))
     
     markup.add(types.InlineKeyboardButton("✅Tekshirish✅", callback_data="check"))
 
-    bot.send_message(chat_id, "Botdan foydalanish uchun kanallarga obuna bo'lishiniz kerak!", reply_markup=markup)
+    await bot.send_message(chat_id, "Botdan foydalanish uchun kanallarga obuna bo'lishiniz kerak!", reply_markup=markup)
 
-def kinolar(message):
-    bot.reply_to(message,f"""Salom!Men Animelarni qisqa treyleylarini topib beruvchi botman.""")
+async def kinolar(message):
+    await bot.reply_to(message,f"""Salom!Men Animelarni qisqa treyleylarini topib beruvchi botman.""")
 
     markup = types.InlineKeyboardMarkup()
 
@@ -52,121 +54,121 @@ def kinolar(message):
     markup.add(button9)
     markup.add(button10)
 
-    bot.send_message(message.chat.id, "Buttonlardan birini tanlang", reply_markup=markup)
+    await bot.send_message(message.chat.id, "Buttonlardan birini tanlang", reply_markup=markup)
 
 @bot.message_handler(commands=['start'])
-def start(message):
+async def start(message):
     user_id = message.from_user.id
-    if check_user(user_id):
-        bot.send_message(message.chat.id, "Botdan foydalanishingiz mumikin!")
-        kinolar(message)
+    if await check_user(user_id):
+        await bot.send_message(message.chat.id, "Botdan foydalanishingiz mumikin!")
+        await kinolar(message)
     else:
-        IsSubscribe(message.chat.id)
+        await IsSubscribe(message.chat.id)
 
 @bot.callback_query_handler(func=lambda call: call.data == "check")
-def check_callback(call):
+async def check_callback(call):
     user_id = call.from_user.id
-    if check_user(user_id):
-        bot.send_message(call.message.chat.id, "Botdan foydalanishingiz mumkin.")
-        bot.answer_callback_query(call.id)
+    if await check_user(user_id):
+        await bot.send_message(call.message.chat.id, "Botdan foydalanishingiz mumkin.")
+        await bot.answer_callback_query(call.id)
     else:
-        bot.send_message(call.message.chat.id, "Barcha kanallarga obuna bo'lmagansiz!\nOBUNA BO'LING!")
-        bot.answer_callback_query(call.id)
+        await bot.send_message(call.message.chat.id, "Barcha kanallarga obuna bo'lmagansiz!\nOBUNA BO'LING!")
+        await bot.answer_callback_query(call.id)
 
 @bot.message_handler(func=lambda message: True)
-def send(message):
+async def send(message):
     user_id = message.from_user.id
-    if not check_user(user_id):
-        IsSubscribe(message.chat.id)
+    if not await check_user(user_id):
+        await IsSubscribe(message.chat.id)
         return
 
     if message.text == "/1":
         a = open("s.mp4","rb")
-        bot.send_video(message.chat.id, a)
-        bot.answer_callback_query(call.id)
+        await bot.send_video(message.chat.id, a)
+        await bot.answer_callback_query(call.id)
     elif message.text == "/2":
         b = open("Blue Lock.mp4","rb")
-        bot.send_video(message.chat.id, b)
-        bot.answer_callback_query(call.id)
+        await bot.send_video(message.chat.id, b)
+        await bot.answer_callback_query(call.id)
     elif message.text == "/3":
         c = open("Shadow.mp4","rb")
-        bot.send_video(message.chat.id, c)
-        bot.answer_callback_query(call.id)
+        await bot.send_video(message.chat.id, c)
+        await bot.answer_callback_query(call.id)
     elif message.text == "/4":
         d = open("Jodugarlar Jangi.mp4","rb")
-        bot.send_video(message.chat.id, d)
-        bot.answer_callback_query(call.id)
+        await bot.send_video(message.chat.id, d)
+        await bot.answer_callback_query(call.id)
     elif message.text == "/5":
         e = open("Iblis qotili.mp4","rb")
-        bot.send_video(message.chat.id, e)
-        bot.answer_callback_query(call.id)
+        await bot.send_video(message.chat.id, e)
+        await bot.answer_callback_query(call.id)
     elif message.text == "/6":
         f = open("Naruto.mp4","rb")
-        bot.send_video(message.chat.id, f)
-        bot.answer_callback_query(call.id)
+        await bot.send_video(message.chat.id, f)
+        await bot.answer_callback_query(call.id)
     elif message.text == "/7":
         g = open("Quvib o'tish.mp4","rb")
-        bot.send_video(message.chat.id, g)
-        bot.answer_callback_query(call.id)
+        await bot.send_video(message.chat.id, g)
+        await bot.answer_callback_query(call.id)
     elif message.text == "/8":
         h = open("Shamolni o'rgatish.mp4","rb")
-        bot.send_video(message.chat.id, h)
-        bot.answer_callback_query(call.id)
+        await bot.send_video(message.chat.id, h)
+        await bot.answer_callback_query(call.id)
     elif message.text == "/9":
         i = open("Xarobalar qirolligi.mp4","rb")
-        bot.send_video(message.chat.id, i)
-        bot.answer_callback_query(call.id)
+        await bot.send_video(message.chat.id, i)
+        await bot.answer_callback_query(call.id)
     elif message.text == "/10":
         j = open("Titanlar hujumi.mp4","rb")
-        bot.send_video(message.chat.id, j)
-        bot.answer_callback_query(call.id)
+        await bot.send_video(message.chat.id, j)
+        await bot.answer_callback_query(call.id)
 
 @bot.callback_query_handler(func=lambda callback:True)
-def nimadir(call):
+async def nimadir(call):
     user_id = call.from_user.id
-    if check_user(user_id):
+    if await check_user(user_id):
         if call.data == "a":
             a = open("s.mp4","rb")
-            bot.send_video(call.message.chat.id, a)
-            bot.answer_callback_query(call.id)
+            await bot.send_video(call.message.chat.id, a)
+            await bot.answer_callback_query(call.id)
         elif call.data == "b":
             b = open("Blue Lock.mp4","rb")
-            bot.send_video(call.message.chat.id, b)
-            bot.answer_callback_query(call.id)
+            await bot.send_video(call.message.chat.id, b)
+            await bot.answer_callback_query(call.id)
         elif call.data == "c":
             c = open("Shadow.mp4","rb")
-            bot.send_video(call.message.chat.id, c)
-            bot.answer_callback_query(call.id)
+            await bot.send_video(call.message.chat.id, c)
+            await bot.answer_callback_query(call.id)
         elif call.data == "d":
             d = open("Jodugarlar Jangi.mp4","rb")
-            bot.send_video(call.message.chat.id, d)
-            bot.answer_callback_query(call.id)   
+            await bot.send_video(call.message.chat.id, d)
+            await bot.answer_callback_query(call.id)   
         elif call.data == "e":
             e = open("Iblis qotili.mp4","rb")
-            bot.send_video(call.message.chat.id, e)
-            bot.answer_callback_query(call.id)      
+            await bot.send_video(call.message.chat.id, e)
+            await bot.answer_callback_query(call.id)      
         elif call.data == "f":
             f = open("Naruto.mp4","rb")
-            bot.send_video(call.message.chat.id, f)
-            bot.answer_callback_query(call.id)  
+            await bot.send_video(call.message.chat.id, f)
+            await bot.answer_callback_query(call.id)  
         elif call.data == "g":
             g = open("Quvib o'tish.mp4","rb")
-            bot.send_video(call.message.chat.id, g)
-            bot.answer_callback_query(call.id) 
+            await bot.send_video(call.message.chat.id, g)
+            await bot.answer_callback_query(call.id) 
         elif call.data == "h":
             h = open("Shamolni o'rgatish.mp4","rb")
-            bot.send_video(call.message.chat.id, h)
-            bot.answer_callback_query(call.id)
+            await bot.send_video(call.message.chat.id, h)
+            await bot.answer_callback_query(call.id)
         elif call.data == "i":
             i = open("Xarobalar qirolligi.mp4","rb")
-            bot.send_video(call.message.chat.id, i)
-            bot.answer_callback_query(call.id)   
+            await bot.send_video(call.message.chat.id, i)
+            await bot.answer_callback_query(call.id)   
         elif call.data == "j":
             j = open("Titanlar hujumi.mp4","rb")
-            bot.send_video(call.message.chat.id, j)
-            bot.answer_callback_query(call.id)
+            await bot.send_video(call.message.chat.id, j)
+            await bot.answer_callback_query(call.id)
     else:
-        IsSubscribe(call.message.chat.id)
+        await IsSubscribe(call.message.chat.id)
 
 print("Iwladi")
-bot.infinity_polling()
+asyncio.run(bot.polling())
